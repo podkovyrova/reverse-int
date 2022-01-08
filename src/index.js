68481bdfa86c1bd7ac00e0bd50d172851023d781
Katerina <podkovyrova.es@mail.ru>
2022-01-08 21:00:05 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+  let stringNumber = String(number);
+
+  if (stringNumber.includes('-')) {
+      stringNumber = stringNumber.slice(1);
+  }
+
+  return stringNumber.split('').reverse().join('');
 }
